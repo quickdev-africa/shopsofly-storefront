8ef316f02537d1e080c4991ce5758cca1977a9f5
WebMastser <info.laserstarglobal@.com>
2026-04-01 21:48:00 +0100

    fix: add React import to landing page components, rewrite StickyBuyNow

--- src/components/landing-pages/ProductSpotlightTemplate.tsx
+++ src/components/landing-pages/ProductSpotlightTemplate.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { useState } from "react";
+import React, { useState } from "react";
 import Image from "next/image";
 import Link from "next/link";
 import { useAppDispatch } from "@/lib/hooks/redux";
